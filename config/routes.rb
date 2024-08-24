c92363818e74b94ef99599b3bc587172e627a873
nayu <19748682+nayu7u@users.noreply.github.com>
2024-08-24 17:29:55 +0000

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   get "trigger/index"
   post "trigger/create"
   # enable mission_control-jobs
@@ -16,5 +17,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
